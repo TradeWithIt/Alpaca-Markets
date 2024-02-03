f503a20ee637a72a4be54b5e5727dd669d261035
Szymon Lorenz <szymon.lorenz.solutions@gmail.com>
2024-02-04 08:33:47 +1000

    Add auth middlewares

--- Sources/AlpacaBroker/Broker.swift
+++ Sources/AlpacaBroker/Broker.swift
@@ -1,15 +1,65 @@
 import OpenAPIRuntime
 import OpenAPIAsyncHTTPClient
+import HTTPTypes
 import Foundation
 
 public struct Broker {
-    public static let sandbox = Client(
-        serverURL: URL(string: "https://broker-api.sandbox.alpaca.markets")!,
-        transport: AsyncHTTPClientTransport()
-    )
+    private static var shared: Broker = .init()
     
-    public static let production = Client(
-        serverURL: URL(string: "https://broker-api.alpaca.markets")!,
-        transport: AsyncHTTPClientTransport()
-    )
+    private var sandboxAuthMiddleware: AuthenticationMiddleware? = nil
+    private var productionAuthMiddleware: AuthenticationMiddleware? = nil
+    
+    private var sandboxClient: Client {
+        Client(
+            serverURL: URL(string: "https://broker-api.sandbox.alpaca.markets")!,
+            transport: AsyncHTTPClientTransport(),
+            middlewares: sandboxAuthMiddleware != nil ? [sandboxAuthMiddleware!] : []
+        )
+    }
+
+    private var productionClient: Client {
+        Client(
+            serverURL: URL(string: "https://broker-api.alpaca.markets")!,
+            transport: AsyncHTTPClientTransport(),
+            middlewares: productionAuthMiddleware != nil ? [productionAuthMiddleware!] : []
+        )
+    }
+    
+    // MARK: Public Access
+    
+    public static var sandbox: Client {
+        shared.sandboxClient
+    }
+    
+    public static var production: Client {
+        shared.productionClient
+    }
+    
+    public static func setSandboxBasicAuth(username: String, password: String) {
+        shared.sandboxAuthMiddleware = AuthenticationMiddleware(username: username, password: password)
+    }
+    
+    public static func setProductionBasicAuth(username: String, password: String) {
+        shared.productionAuthMiddleware = AuthenticationMiddleware(username: username, password: password)
+    }
+}
+
+// MARK: Helpers
+
+private struct AuthenticationMiddleware: ClientMiddleware {
+    var username: String
+    var password: String
+
+    func intercept(
+        _ request: HTTPRequest,
+        body: HTTPBody?,
+        baseURL: URL,
+        operationID: String,
+        next: (HTTPRequest, HTTPBody?, URL
+        ) async throws -> (HTTPResponse, HTTPBody?)) async throws -> (HTTPResponse, HTTPBody?) {
+        var request = request
+        let base64 = try "\(username):\(password)".base64Decoded()
+        request.headerFields[.authorization] = "Basic \(base64)"
+        return try await next(request, body, baseURL)
+    }
 }
--- Sources/AlpacaMarket/Market.swift
+++ Sources/AlpacaMarket/Market.swift
@@ -1,15 +1,69 @@
 import OpenAPIRuntime
 import OpenAPIAsyncHTTPClient
+import HTTPTypes
 import Foundation
 
 public struct Market {
-    public static let sandbox = Client(
-        serverURL: URL(string: "https://data.sandbox.alpaca.markets")!,
-        transport: AsyncHTTPClientTransport()
-    )
+    private static var shared: Market = .init()
+    
+    private var sandboxAuthMiddleware: AuthenticationMiddleware? = nil
+    private var productionAuthMiddleware: AuthenticationMiddleware? = nil
+    
+    private var sandboxClient: Client {
+        Client(
+            serverURL: URL(string: "https://data.sandbox.alpaca.markets")!,
+            transport: AsyncHTTPClientTransport(),
+            middlewares: sandboxAuthMiddleware != nil ? [sandboxAuthMiddleware!] : []
+        )
+    }
 
-    public static let production = Client(
-        serverURL: URL(string: "https://data.alpaca.markets")!,
-        transport: AsyncHTTPClientTransport()
-    )
+    private var productionClient: Client {
+        Client(
+            serverURL: URL(string: "https://data.alpaca.markets")!,
+            transport: AsyncHTTPClientTransport(),
+            middlewares: productionAuthMiddleware != nil ? [productionAuthMiddleware!] : []
+        )
+    }
+    
+    // MARK: Public Access
+    
+    public static var sandbox: Client {
+        shared.sandboxClient
+    }
+    
+    public static var production: Client {
+        shared.productionClient
+    }
+    
+    public static func setSandboxApiKeys(apiKey: String, apiSecret: String) {
+        shared.sandboxAuthMiddleware = AuthenticationMiddleware(apiKey: apiKey, apiSecret: apiSecret)
+    }
+    
+    public static func setProductionApiKeys(apiKey: String, apiSecret: String) {
+        shared.productionAuthMiddleware = AuthenticationMiddleware(apiKey: apiKey, apiSecret: apiSecret)
+    }
+}
+
+// MARK: Helpers
+
+private struct AuthenticationMiddleware: ClientMiddleware {
+    var apiKey: String
+    var apiSecret: String
+
+    func intercept(
+        _ request: HTTPRequest,
+        body: HTTPBody?,
+        baseURL: URL,
+        operationID: String,
+        next: (HTTPRequest, HTTPBody?, URL
+        ) async throws -> (HTTPResponse, HTTPBody?)) async throws -> (HTTPResponse, HTTPBody?) {
+        var request = request
+        if let name = HTTPField.Name("APCA-API-KEY-ID") {
+            request.headerFields[name] = apiKey
+        }
+        if let name = HTTPField.Name("APCA-API-SECRET-KEY") {
+            request.headerFields[name] = apiSecret
+        }
+        return try await next(request, body, baseURL)
+    }
 }
